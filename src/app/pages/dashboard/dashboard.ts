c1fd7df86c0a5376be2b0f8f313b236c15c3620b
Mandeep-Heir <mandeepheir11@gmail.com>
2026-09-24 06:55:20 -0400

    Complete Day 6 dynamic dashboard statistics

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Application } from '../../models/application';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,80 @@ import { RouterLink } from '@angular/router';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+
+  applications: Application[] = [];
+
+  totalApplications = 0;
+
+  interviews = 0;
+
+  offers = 0;
+
+  rejected = 0;
+
+
+  ngOnInit() {
+
+    this.loadApplications();
+
+  }
+
+
+  loadApplications() {
+
+    const savedApplications =
+      localStorage.getItem('applications');
+
+    if (savedApplications) {
+
+      this.applications =
+        JSON.parse(savedApplications);
+
+    }
+
+    this.calculateStats();
+
+  }
+
+
+  calculateStats() {
+
+    this.totalApplications =
+      this.applications.length;
+
+
+    this.interviews =
+      this.applications.filter(
+        application =>
+          application.status === 'Interview' ||
+          application.status === 'Technical'
+      ).length;
+
+
+    this.offers =
+      this.applications.filter(
+        application =>
+          application.status === 'Offer'
+      ).length;
+
+
+    this.rejected =
+      this.applications.filter(
+        application =>
+          application.status === 'Rejected'
+      ).length;
+
+  }
+
+
+  getRecentApplications(): Application[] {
+
+    return this.applications
+      .slice()
+      .reverse()
+      .slice(0, 5);
+
+  }
+
 }
