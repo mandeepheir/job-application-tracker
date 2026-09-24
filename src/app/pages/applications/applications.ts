9e72f5a486beb85bc51db404aea2b12e8bc4d1ad
Mandeep-Heir <mandeepheir11@gmail.com>
2026-09-24 06:50:22 -0400

    Complete Day 5 search and filter functionality

--- src/app/pages/applications/applications.ts
+++ src/app/pages/applications/applications.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Application } from '../../models/application';
 
 @Component({
   selector: 'app-applications',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './applications.html',
   styleUrl: './applications.css'
 })
@@ -12,16 +13,79 @@ export class Applications {
 
   applications: Application[] = [];
 
+  filteredApplications: Application[] = [];
+
+  searchTerm = '';
+
+  selectedStatus = '';
+
+  selectedJobType = '';
+
   ngOnInit() {
 
     const savedApplications =
       localStorage.getItem('applications');
 
     if (savedApplications) {
+
       this.applications = JSON.parse(savedApplications);
+
     }
 
+    this.filterApplications();
+
   }
+
+
+  filterApplications() {
+
+    this.filteredApplications = this.applications.filter(
+      application => {
+
+        const matchesSearch =
+          application.company
+            .toLowerCase()
+            .includes(this.searchTerm.toLowerCase()) ||
+
+          application.jobTitle
+            .toLowerCase()
+            .includes(this.searchTerm.toLowerCase());
+
+
+        const matchesStatus =
+          !this.selectedStatus ||
+          application.status === this.selectedStatus;
+
+
+        const matchesJobType =
+          !this.selectedJobType ||
+          application.jobType === this.selectedJobType;
+
+
+        return (
+          matchesSearch &&
+          matchesStatus &&
+          matchesJobType
+        );
+
+      }
+    );
+
+  }
+
+
+  clearFilters() {
+
+    this.searchTerm = '';
+
+    this.selectedStatus = '';
+
+    this.selectedJobType = '';
+
+    this.filterApplications();
+
+  }
+
 
   deleteApplication(index: number) {
 
@@ -33,12 +97,29 @@ export class Applications {
       return;
     }
 
-    this.applications.splice(index, 1);
+
+    const applicationToDelete =
+      this.filteredApplications[index];
+
+
+    const originalIndex =
+      this.applications.indexOf(applicationToDelete);
+
+
+    if (originalIndex !== -1) {
+
+      this.applications.splice(originalIndex, 1);
+
+    }
+
 
     localStorage.setItem(
       'applications',
       JSON.stringify(this.applications)
     );
+
+
+    this.filterApplications();
 
   }
 
